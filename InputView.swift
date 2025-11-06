e7af98ecfd9acde35e2a5b90ace37fad4b737cbc
Jackson Lisle <jackson.lisle26@d155.org>
2025-11-06 08:10:26 -0600

    workday1

---
+++ InputView.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView.swift
+//  gardenTrackerLisle
+//
+//  Created by JACKSON LISLE on 11/6/25.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    
+    @State var 
+    
+    var body: some View {
+
+        
+        
+        
+
+
+    }
+}
+
+#Preview {
+    InputView()
+}
---
+++ PlantVariety.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  gardenTrackerLisle
+//
+//  Created by JACKSON LISLE on 11/5/25.
+//
+
+import Foundation
+
+class Plant{
+    
+    var name: String
+    var 
+    
+    
+    
+    
+    
+    
+}
+
+
